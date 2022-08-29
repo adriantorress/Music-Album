2d7d1bc1d4016d1060792bc1b48b6c4717380ede
adriantorress <adriantorrescv@gmail.com>
2022-08-29 04:08:19 -0300

    Atualizações no sistema de rotas e na userpage.

--- app.py
+++ app.py
@@ -12,48 +12,43 @@ def homepage():
 
 @app.route('/register', methods=['POST'])
 def register():
-    global userData, sign_up_response
+    global userData, sign_up_response, come_from
     userData, sign_up_response = sign_up()
+    come_from = "register"
     if sign_up_response == "Usuário cadastrado com sucesso!":
         return redirect(url_for('user'), code=307)
     else:
-        return redirect(url_for('signUp'), code=307)
+        return redirect(url_for('sign'), code=307)
 
 
 @app.route('/authentication', methods=['POST'])
 def authentication():
-    global userData, sign_in_response
+    global userData, sign_in_response, come_from
     sign_in_response = sign_in()
+    come_from = "authentication"
     if type(sign_in_response) == list:
         global userData
         userData = {
             "firstName": sign_in_response[0], "lastName": sign_in_response[1]}
         return redirect(url_for('user'), code=307)
     else:
-        return redirect(url_for('signIn'), code=307)
+        return redirect(url_for('sign'), code=307)
 
 
 @app.route('/sign', methods=['GET', 'POST'])
-def signUp():
+def sign():
     if request.method == 'GET':
         return render_template('sign.html')
-    elif request.method == 'POST':
+    elif request.method == 'POST' and come_from == "register":
         return render_template('sign.html', sign_up_response=sign_up_response)
-
-
-@app.route('/sign-in', methods=['GET', 'POST'])
-def signIn():
-    if request.method == 'GET':
-        return render_template('sign.html')
-    elif request.method == 'POST':
+    elif request.method == 'POST' and come_from == "authentication":
         return render_template('sign.html', sign_in_response=sign_in_response)
 
 
 @app.route('/user', methods=['GET', 'POST'])
 def user():
     if request.method == 'GET':
-        titleName = userData["firstName"] + " " + userData["lastName"]
-        return render_template('user.html', titleName=titleName)
+        return redirect(url_for('sign'))
     elif request.method == 'POST':
         titleName = userData["firstName"] + " " + userData["lastName"]
         return render_template('user.html', titleName=titleName)
