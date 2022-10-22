8293ece6997ff4f0465ec9cae9eb956fb96b950b
adriantorress <adriantorrescv@gmail.com>
2022-10-21 23:56:35 -0300

    tentativa 4

--- back_end/app.js
+++ back_end/app.js
@@ -26,7 +26,6 @@ try {
     .then(() => {
       console.log('MongoDB is connected!');
       var port_number = server.listen(process.env.PORT || 3000);
-      app.listen(port_number);
       app.listen(port_number, () => {
         console.log('Server is running!')
       })
